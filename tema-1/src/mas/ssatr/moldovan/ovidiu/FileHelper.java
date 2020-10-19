394771395112247984f6071e041cebc5b1d276a0
Ovidiu Moldovan <ovi1602@gmail.com>
2020-10-19 23:45:22 +0300

    tema-1 Delete Previous Output

--- tema-1/src/mas/ssatr/moldovan/ovidiu/FileHelper.java
+++ tema-1/src/mas/ssatr/moldovan/ovidiu/FileHelper.java
@@ -1,13 +1,24 @@
 package mas.ssatr.moldovan.ovidiu;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHelper {
+
+    private static String fileName = "output.txt";
+
+    public static void DeletePreviousOutputs(){
+        File myObj = new File(fileName);
+        if (myObj.delete()) {
+            System.out.println("New file printed");
+        }
+    }
+
     public static void Write(String content) {
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt", true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true))) {
             bw.write(content);
             bw.newLine();
         } catch (IOException e) {
--- tema-1/src/mas/ssatr/moldovan/ovidiu/Main.java
+++ tema-1/src/mas/ssatr/moldovan/ovidiu/Main.java
@@ -10,6 +10,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        FileHelper.DeletePreviousOutputs();
         var Loader = new PetriNetLoader();
         Loader.read();
         var PetriNetSimulator = new PetriNetSimulator(Loader.petriNetModel);
